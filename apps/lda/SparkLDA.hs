1047291100c52582609c908c875f37ec03b2b88f
Alp Mestanogullari <alp.mestanogullari@tweag.io>
2016-06-13 11:51:53 +0200

    Changes to make sparkle's lda example run on databricks

--- apps/lda/SparkLDA.hs
+++ apps/lda/SparkLDA.hs
@@ -7,12 +7,12 @@ import Control.Distributed.Spark
 main :: IO ()
 main = do
     conf <- newSparkConf "Spark Online Latent Dirichlet Allocation in Haskell!"
-    confSet conf "spark.hadoop.fs.s3n.awsAccessKeyId" "AKIAIKSKH5DRWT5OPMSA"
-    confSet conf "spark.hadoop.fs.s3n.awsSecretAccessKey" "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"
-    sc   <- getOrCreate conf
-    sqlc <- newSQLContext sc
-    stopwords <- textFile sc "s3n://tweag-sparkle/stopwords.txt" >>= collect
-    docs <- wholeTextFiles sc "s3n://tweag-sparkle/nyt/"
+    -- confSet conf "spark.hadoop.fs.s3a.awsAccessKeyId" "AKIAIKSKH5DRWT5OPMSA"
+    -- confSet conf "spark.hadoop.fs.s3a.awsSecretAccessKey" "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"
+    sc   <- getOrCreateSparkContext conf
+    sqlc <- getOrCreateSQLContext sc
+    stopwords <- textFile sc "s3a://AKIAIKSKH5DRWT5OPMSA:bmTL4A9MubJSV9Xhamhi5asFVllhb8y10MqhtVDD@tweag-sparkle/stopwords.txt" >>= collect
+    docs <- wholeTextFiles sc "s3a://AKIAIKSKH5DRWT5OPMSA:bmTL4A9MubJSV9Xhamhi5asFVllhb8y10MqhtVDD@tweag-sparkle/nyt/"
         >>= justValues
         >>= zipWithIndex
     docsRows <- toRows docs
