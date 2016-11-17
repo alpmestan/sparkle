b3c6230deb1031c357b6f9949c10abbe44bf06de
Alp Mestanogullari <alpmestan@gmail.com>
2016-11-17 21:30:48 +0100

    add benchmarking script variant used on the cluster

---
+++ bench-cluster.sh
@@ -0,0 +1,23 @@
+export DATASET_SIZE=10000
+export DATASET_PARTITIONS=16
+
+SPARK_SUBMIT="spark-submit --driver-memory 3g --executor-memory 2g --conf spark.yarn.executor.memoryOverhead=3g --conf spark.dynamicAllocation.enabled=false --num-executors 4"
+
+rm -r bench-*.jar*
+
+for n in 1 2 4; do
+    rm -r /tmp/blockmgr* /tmp/app* /tmp/spark*
+    for v in micro-java.jar micro-patched-sparkle.jar micro-nonpatched-sparkle.jar; do
+	SPARK_CMD=""
+	if [ $v = "micro-java.jar" ]; then
+	    SPARK_CMD="$SPARK_SUBMIT --executor-cores $n --class Micro $v"
+	else
+	    SPARK_CMD="$SPARK_SUBMIT --executor-cores $n $v"
+	fi
+	for i in $(seq 1); do
+	    echo "Running $v on $n cores, sample $i"
+	    # echo $SPARK_CMD
+	    $SPARK_CMD 2>&1 | grep "Job.*finished" | sed -e 's/took /\n/g' | tail -1 | sed -e 's/,/./g' | sed -e 's/ /\n/g' | head -1 >> bench-$v-$n-cores
+	done
+    done
+done
